47d51a336eaac19b84fc68c517afc4c81eb41235
Gülistan Kumaş <139699496+gulistankumas@users.noreply.github.com>
2023-09-14 00:49:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() >320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    #right paddle topu kaçırdıysa
+    #right paddle topu kaçırdımı
 
     if ball.xcor() >380:
         ball.reset_position()
